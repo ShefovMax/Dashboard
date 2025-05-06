1b46195d54c03cad54e0b7217b8ea1f0c65f38ea
Pe4all <shefov.max@yandex.ru>
2025-05-06 22:17:56 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App:React.FC = () => {
+  return <div>123112 2</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
